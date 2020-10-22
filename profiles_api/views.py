ef70905cd58f6fb295a663d07e32df52c5bcd75a
davidverduin <davidverduincortes@gmail.com>
2020-10-22 22:53:35 +0200

    Added search option to profiles viewset

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -3,6 +3,7 @@ from rest_framework.response import Response
 from rest_framework import status
 from rest_framework import viewsets
 from rest_framework.authentication import TokenAuthentication
+from rest_framework import filters
 
 from profiles_api import serializers
 from profiles_api import models
@@ -109,4 +110,6 @@ class UserProfileViewSet(viewsets.ModelViewSet):
     queryset = models.UserProfile.objects.all()
     authentication_classses = (TokenAuthentication,)
     permission_classes = (permissions.UpdateOwnProfile, )
-        
+    # DJango adds automatically the search option by the given fields
+    filter_backends = (filters.SearchFilter, )
+    search_fields = ('name', 'email', )
